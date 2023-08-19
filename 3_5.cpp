a03ae5909c5f47d17f4ce52ba51d722cebd17db6
Bowen Yi <123020250+bowen20190101@users.noreply.github.com>
2023-08-19 17:05:26 -0400

    Updated

--- 3_5.cpp
+++ 3_5.cpp
@@ -7,39 +7,58 @@
 
 using namespace std;
 
-// Overly complicated
 void sort_stack(stack<int>& input){
   stack<int> sorted;
   int temp;
   while(!input.empty()){
     temp = input.top();
-    // if sorted is empty || if temp is greater or equal to sorted.top()
-    if(sorted.empty() || temp >= sorted.top()){
-      sorted.push(temp);
-      input.pop();
-    } else {
-      input.pop();
-      int counter = 0;
-      while(!sorted.empty() && temp < sorted.top()){
-        input.push(sorted.top());
-        sorted.pop();
-        counter++;
-      }
-      sorted.push(temp);
-      while(counter--){
-        sorted.push(input.top());
-        input.pop();
-      }
+    input.pop();
+    while(!sorted.empty() && temp < sorted.top()){
+      input.push(sorted.top());
+      sorted.pop();
     }
+    sorted.push(temp);
   }
-
   while(!sorted.empty()){
     input.push(sorted.top());
     sorted.pop();
   }
-
   return;
 }
+
+// Overly complicated
+// void sort_stack(stack<int>& input){
+//   stack<int> sorted;
+//   int temp;
+//   while(!input.empty()){
+//     temp = input.top();
+//     // if sorted is empty || if temp is greater or equal to sorted.top()
+//     if(sorted.empty() || temp >= sorted.top()){
+//       sorted.push(temp);
+//       input.pop();
+//     } else {
+//       input.pop();
+//       int counter = 0;
+//       while(!sorted.empty() && temp < sorted.top()){
+//         input.push(sorted.top());
+//         sorted.pop();
+//         counter++;
+//       }
+//       sorted.push(temp);
+//       while(counter--){
+//         sorted.push(input.top());
+//         input.pop();
+//       }
+//     }
+//   }
+
+//   while(!sorted.empty()){
+//     input.push(sorted.top());
+//     sorted.pop();
+//   }
+
+//   return;
+// }
 
 int main(){
   stack<int> test;
